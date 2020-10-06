4fa3151878a40f0f2a7777cb32947ed6d65695b7
tyugv <milsocol@gmail.com>
2020-10-06 15:05:41 +0300

    no cache

--- app.py
+++ app.py
@@ -32,6 +32,16 @@ def refresh_plot():
     plt.close(fig)
 
 
+@app.after_request
+def add_header(r):
+
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 @app.route('/', methods=['GET', 'POST'])
 def loss_plotting():
 
