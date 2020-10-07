bc7c9900fe2c1de87b381d170862ecbc1711a1ac
tyugv <milsocol@gmail.com>
2020-10-07 14:24:27 +0300

    add learning rates

--- app.py
+++ app.py
@@ -8,18 +8,26 @@ app = Flask(__name__)
 def get_data(data, header, arr):
     if header in data:
         try:
-            loss_item = float(data[header])
-            arr.append(loss_item)
+            item = float(data[header])
+            arr.append(item)
 
+        except ValueError:
+            print('get value not as number')
+
+
+def change_data(data, header, el):
+    if header in data:
+        try:
+            el = float(data[header])
         except ValueError:
             print('get value not as number')
 
 
 class Learning:
 
-    def __init__(self, app, params=None):
+    def __init__(self, params=None):
         if params is None:
-            self.params = {'max': [], 'mean': [], 'min': [], 'lr': [0]}
+            self.params = {'max': [], 'mean': [], 'min': [], 'lr': 0, 'cur_lr': 0}
         else:
             self.params = params
 
@@ -28,6 +36,8 @@ class Learning:
         plt.plot(self.params['max'])
         plt.plot(self.params['mean'])
         plt.plot(self.params['min'])
+        lr = app.model.params['cur_lr']
+        plt.title(f'Loss plots with lr = {lr}')
         plt.savefig('static/loss_plot.png', format='png')
         plt.close(fig)
 
@@ -35,7 +45,8 @@ class Learning:
         get_data(data, 'mean_loss', self.params['mean'])
         get_data(data, 'min_loss', self.params['min'])
         get_data(data, 'max_loss', self.params['max'])
-        get_data(data, 'lr', self.params['lr'])
+        change_data(data, 'lr', self.params['lr'])
+        change_data(data, 'cur_lr', self.params['cur_lr'])
         self.refresh_plot()
 
 
@@ -54,11 +65,14 @@ def index():
     try:
         app.model
     except AttributeError:
-        app.model = Learning(app)
+        app.model = Learning()
 
     if request.method == 'POST':
         data = request.form.to_dict()
         app.model.refresh_params(data)
+        if app.model.params['lr'] != app.model.params['cur_lr']:
+            return render_template('loss.html', url='static/loss_plot.png'), 200, \
+                   {'lr': app.model.params['lr']}
 
     return render_template('loss.html', url='static/loss_plot.png')
 
@@ -66,7 +80,7 @@ def index():
 @app.route('/new_learning', methods=['POST'])
 def create_new_learning():
     del app.model
-    app.model = Learning(app)
+    app.model = Learning()
     return redirect(url_for('index'))
 
 
