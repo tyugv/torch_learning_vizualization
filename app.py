d8957a55083369848e7ef8f67a8c3b5bc2e79acc
tyugv <milsocol@gmail.com>
2020-10-07 13:58:51 +0300

    changes

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import matplotlib.pyplot as plt
-from flask import Flask, jsonify, request, render_template, Response, send_file
+from flask import Flask, request, render_template, redirect, url_for
 import pickle
 
 app = Flask(__name__)
 
-app.params = {
-    'max': [],
-    'mean': [],
-    'min': [],
-    'lr': [0]
-}
 
 def get_data(data, header, arr):
-
     if header in data:
         try:
             loss_item = float(data[header])
@@ -22,13 +15,28 @@ def get_data(data, header, arr):
             print('get value not as number')
 
 
-def refresh_plot():
-    fig = plt.figure()
-    plt.plot(app.params['max'])
-    plt.plot(app.params['mean'])
-    plt.plot(app.params['min'])
-    plt.savefig('static/loss_plot.png', format='png')
-    plt.close(fig)
+class Learning:
+
+    def __init__(self, app, params=None):
+        if params is None:
+            self.params = {'max': [], 'mean': [], 'min': [], 'lr': [0]}
+        else:
+            self.params = params
+
+    def refresh_plot(self):
+        fig = plt.figure()
+        plt.plot(self.params['max'])
+        plt.plot(self.params['mean'])
+        plt.plot(self.params['min'])
+        plt.savefig('static/loss_plot.png', format='png')
+        plt.close(fig)
+
+    def refresh_params(self, data):
+        get_data(data, 'mean_loss', self.params['mean'])
+        get_data(data, 'min_loss', self.params['min'])
+        get_data(data, 'max_loss', self.params['max'])
+        get_data(data, 'lr', self.params['lr'])
+        self.refresh_plot()
 
 
 @app.after_request
@@ -42,22 +50,25 @@ def add_header(r):
 
 
 @app.route('/', methods=['GET', 'POST'])
-def loss_plotting():
+def index():
+    try:
+        app.model
+    except AttributeError:
+        app.model = Learning(app)
 
     if request.method == 'POST':
-
         data = request.form.to_dict()
-
-        get_data(data, 'mean_loss', app.params['mean'])
-        get_data(data, 'min_loss', app.params['min'])
-        get_data(data, 'max_loss', app.params['max'])
-        get_data(data, 'lr', app.params['lr'])
-
-        refresh_plot()
+        app.model.refresh_params(data)
 
     return render_template('loss.html', url='static/loss_plot.png')
 
 
-if __name__ == '__main__':
+@app.route('/new_learning', methods=['POST'])
+def create_new_learning():
+    del app.model
+    app.model = Learning(app)
+    return redirect(url_for('index'))
 
-    app.run()
+
+if __name__ == '__main__':
+    app.run(debug=True)
